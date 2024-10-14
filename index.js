bfd677205e9e32399e2f9b2ebc345a7c5586746f
Rickard Hole Falck <rickardhole.falck@gmail.com>
2024-10-14 16:21:06 +0200

    Added some more output writing, testing with adding new functionality

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post("/", express.json(), async (req, res) => {
   const payload = req.body;
   console.log("Received payload:", payload);
 
-  // Insert a special pirate-y system message in our message list.
+  // Insert system messages in our message list.
   const messages = payload.messages;
   messages.unshift({
     role: "system",
@@ -28,6 +28,51 @@ app.post("/", express.json(), async (req, res) => {
     role: "system",
     content: "In every response you should weave in one of the stanzas from Havamal, the sayings of Odin.",
   });
+
+  console.log("Messages:", messages);
+
+  // Use Copilot's LLM to generate a response to the user's messages, with
+  // our extra system messages attached.
+  const copilotLLMResponse = await fetch(
+    "https://api.githubcopilot.com/chat/completions",
+    {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${tokenForUser}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        messages,
+        stream: true,
+      }),
+    }
+  );
+
+  // Stream the response straight back to the user.
+  Readable.from(copilotLLMResponse.body).pipe(res);
+})
+
+app.post("/runify", express.json(), async (req, res) => {
+  // Identify the user, using the GitHub API token provided in the request headers.
+  const tokenForUser = req.get("X-GitHub-Token");
+  const octokit = new Octokit({ auth: tokenForUser });
+  const user = await octokit.request("GET /user");
+
+  // Parse the request payload and log it.
+  const payload = req.body;
+  console.log("Received payload:", payload);
+
+  // Insert system messages in our message list.
+  const messages = payload.messages;
+  messages.unshift({
+    role: "system",
+    content: "You are a helpful assistant that replies to user messages as if you were Odin the Allfather.",
+  });
+  messages.unshift({
+    role: "system",
+    content: `Take the code provided by the user and change it to using the rune unicode characters where it is possible to do so without rendering the code useless.`,
+  });
+
 
   // Use Copilot's LLM to generate a response to the user's messages, with
   // our extra system messages attached.
